b4cf9d32d2627ad522632f618f47a67e5e2f4f86
gerardmatos <gerardmatos1990@gmail.com>
2024-04-19 06:59:06 -0500

    !feat: change numbers to develzpbx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -81,15 +81,15 @@ export const Header = () => {
         switch (TSource) {
           case '01clarohogar':
             setNumber({
-              tel: '016806301',
-              num: '(01) 6806301'
+              tel: '017028541',
+              num: '(01) 7028541'
             });
             break;
   
           case '02clarointernet':
             setNumber({
-              tel: '016806302',
-              num: '(01) 6806302'
+              tel: '017028545',
+              num: '(01) 7028545'
             });
             break;
   
@@ -102,8 +102,8 @@ export const Header = () => {
   
           case '05claroperu':
             setNumber({
-              tel: '016806545',
-              num: '(01) 6806545'
+              tel: '017017610',
+              num: '(01) 7017610'
             });
             break;
   
